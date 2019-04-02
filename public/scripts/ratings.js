fe4e2731f13a064fa15c0a1b8665e2fdd1619711
Victor Cuc <victorcuc99@yahoo.com>
2019-04-02 14:27:52 +0100

    submission

--- public/scripts/ratings.js
+++ public/scripts/ratings.js
@@ -1,22 +1,18 @@
-// This waits for the DOM to load correctly before changing elements
-$(document).ready(function(){
-  // All your normal JS code goes in here
-  const ratingBar = $('.ui.rating');
-  const ratingValue = $('#rating-value');
-  const submitButton = $('#submit-button');
+$(document).ready(function() {
+	const ratingBar = $('.ui.rating');
+	const ratingValue = $('#rating-value');
+	const submitButton = $('#submit-button');
 
-  ratingValue.val(null);
-  
-  ratingBar
-  .rating()
-  .click(function() {
-    ratingValue.val(ratingBar.rating("get rating"));
-  });
+	ratingValue.val(null);
 
-  submitButton.click(function(e) {
-    if (ratingValue.val() < 1) {
-      e.preventDefault();
-      alert("Please rate!");
-    }
-  });  
+	ratingBar.rating().click(function() {
+		ratingValue.val(ratingBar.rating('get rating'));
+	});
+
+	submitButton.click(function(e) {
+		if (ratingValue.val() < 1) {
+			e.preventDefault();
+			alert('Please rate!');
+		}
+	});
 });
